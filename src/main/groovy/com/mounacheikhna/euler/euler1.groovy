6a2c47e180cdb3eca93bf900b6ce11b449e48900
Mouna Cheikhna <cheikhnamouna@gmail.com>
2015-11-08 13:22:42 +0100

    Add a second solution to euler problem #1.

--- src/main/groovy/com/mounacheikhna/euler/euler1.groovy
+++ src/main/groovy/com/mounacheikhna/euler/euler1.groovy
@@ -8,12 +8,15 @@ import rx.Observable;
  Find the sum of all the multiples of 3 or 5 below 1000.
  **/
 
-def solution1 = { Observable.range(1,999).filter({ !((it % 3) && (it % 5)) }) };
+//Solution #1
+def solution1 = Observable.range(1,999).filter({ !((it % 3) && (it % 5)) })
+def summer = solution1.reduce(0, { a, b -> a+b })
+summer.subscribe({ println(it); })
 
-def threes = Observable.range(1,999).map({it*3}).takeWhile({it<1000});
-def fives = Observable.range(1,999).map({it*5}).takeWhile({it<1000});
-def solution2 = { Observable.merge(threes, fives).distinct() };
 
-solution1().toBlocking().subscribe({ println(it)})
-
-solution2().toBlocking().subscribe({ println(it)})
+//Solution #2
+def threes = Observable.range(1,999).map({it*3}).takeWhile({it<1000})
+def fives = Observable.range(1,999).map({it*5}).takeWhile({it<1000})
+def solution2 = Observable.merge(threes, fives).distinct();
+def summer2 = solution2.scan(0, { a, b -> a+b }).takeLast(1);
+summer2.subscribe({ println(it); })
