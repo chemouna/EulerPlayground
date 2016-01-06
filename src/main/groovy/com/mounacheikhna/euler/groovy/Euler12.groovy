f50aedde345bd6aa143627a4c63b69dd0873ba12
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-01-06 05:54:28 +0100

    Fix Euler #12 in groovy and add Haskell Euler #12 solution.

--- src/main/groovy/com/mounacheikhna/euler/groovy/Euler12.groovy
+++ src/main/groovy/com/mounacheikhna/euler/groovy/Euler12.groovy
@@ -1,48 +1,53 @@
+import groovy.time.TimeCategory
+import groovy.time.TimeDuration
 
 //First naive solution
 
-
 //TODO: convert this to more idiomatic groovy next
-def numDivisors(n) {
-    if(n % 2 == 0) {
+def numDivisors(int n) {
+    if (n % 2 == 0) {
         n = n / 2
     }
 
     divisors = 1
     count = 0
 
-    while(n % 2 == 0) {
+    while (n % 2 == 0) {
         count += 1
-        n = n/2
+        n = n / 2
     }
     divisors = divisors * (count + 1)
     p = 3
-    while(n != 1) {
+    while (n != 1) {
         count = 0
-        while(n % p == 0) {
+        while (n % p == 0) {
             count += 1
             n = n / p
         }
         divisors = divisors * (count + 1)
         p += 2
-    divisors
+        divisors
+    }
 }
 
-def find_triangular_index() {
+def findTriangularIndex(limit) {
     n = 1
-    lnum = num_divisors(n)
-    rnum = num_divisors(n + 1)
-    while (lnum * rnum < 500) {
+    lnum = numDivisors(n)
+    rnum = numDivisors(n + 1)
+    while (lnum * rnum < limit) {
         n += 1
         lnum = rnum
-        rnum = num_divisors(n + 1)
+        rnum = numDivisors(n + 1)
     }
     n
 }
 
-start = time.time()
-index = find_triangular_index(500)
-triangle = (index * (index + 1)) / 2
-elapsed = (time.time() - start)
 
-println "result %s returned in ${triangle} ${elapsed} seconds"
+def timeStart = new Date()
+
+index = findTriangularIndex(500)
+triangle = (index * (index + 1)) / 2
+
+def timeStop = new Date()
+TimeDuration duration = TimeCategory.minus(timeStop, timeStart)
+println "result %s returned in ${triangle} ${duration} seconds"
---
+++ src/main/haskell/Euler12.hs
@@ -0,0 +1,28 @@
+
+import Data.List (group)
+
+triangles :: [Int]
+triangles = scanl1 (+) [1..]
+
+primes :: [Int]
+primes = sieve [2..] where
+    sieve [] = []
+    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p > 0]
+
+factorize :: Int -> [Int]
+factorize n = primeFactors n primes where
+    primeFactors 1 _ = []
+    primeFactors _ [] = []
+    primeFactors m (p:ps) | m < p * p = [m]
+                          | r == 0 = p : primeFactors q (p:ps)
+                          | otherwise = primeFactors m ps
+                          where (q, r) = quotRem m p
+
+primePowers :: Int -> [(Int, Int)]
+primePowers n = [(head x, length x) | x <- group $ factorize n]
+
+numDivisors :: Int ->  Int
+numDivisors n = product [k + 1 | (_, k) <- primePowers n]
+
+main :: IO ()
+main = print $ head $ dropWhile (\n -> numDivisors n <= 500) triangles
